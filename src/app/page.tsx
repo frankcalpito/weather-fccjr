09176dc1f9ce5e4744fd3cef3bf4e4a977d8dcc8
Franklin Calpito <frankcalpitojr@gmail.com>
2024-09-13 19:50:39 -0600

    Add loader while fetching location weather details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,31 @@
 "use client";
 
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../lib/store";
 import {
   addLocation,
   fetchWeatherForLocation,
 } from "../lib/features/location/locationSlice";
 import WeatherCard from "../components/WeatherCard/WeatherCard";
 import SearchBar from "../components/SearchBar/SearchBar";
-import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import Spinner from "../components/Spinner/Spinner";
 
 const Home = () => {
-  const locations = useAppSelector((state) => state.location.locations);
-  const dispatch = useAppDispatch();
+  const locations = useSelector((state: RootState) => state.location.locations);
+  const dispatch: AppDispatch = useDispatch();
 
-  const handleAddLocation = async (location: {
+  // Track loading state for weather data
+  const weatherLoading = locations.some((loc) => loc.weather === undefined);
+
+  const handleAddLocation = (location: {
     name: string;
     lat: number;
     long: number;
   }) => {
+    // Add location to Redux store
     dispatch(addLocation(location));
+
+    // Fetch weather data for the location
     dispatch(fetchWeatherForLocation(location));
   };
 
@@ -25,19 +33,23 @@ const Home = () => {
     <div className="container mx-auto min-h-screen p-10">
       <SearchBar onLocationSelected={handleAddLocation} />
       <div className="mt-8 space-y-4">
-        {locations.map((location, index) => (
-          <div key={index}>
-            {!location.weather ? (
-              <p>Loading weather for {location.name}...</p>
-            ) : (
-              <WeatherCard
-                city={location.name}
-                temp={location.weather.main.temp}
-                description={location.weather.weather[0].description}
-              />
-            )}
-          </div>
-        ))}
+        {weatherLoading ? (
+          <Spinner /> // Show spinner while weather data is loading
+        ) : (
+          locations.map((location, index) => (
+            <div key={index}>
+              {location.weather ? (
+                <WeatherCard
+                  city={location.name}
+                  temp={location.weather.main.temp}
+                  description={location.weather.weather[0].description}
+                />
+              ) : (
+                <p>Loading weather for {location.name}...</p>
+              )}
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-32">
+      <div className="border-t-transparent border-solid animate-spin border-blue-500 border-8 rounded-full h-16 w-16" />
+    </div>
+  );
+};
+
+export default Spinner;
